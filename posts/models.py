63745435e1bc58250cc2bce427dccc52ab8ec7a8
myrzatovnurgeldi@gmail.com <myrzatovnurgeldi@gmail,com>
2022-11-22 13:10:16 +0600

    django!

--- posts/models.py
+++ posts/models.py
@@ -25,4 +25,3 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     def __str__(self):
         return f'{self.author.username}_{self.text}'
-
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from posts import views
 
-
 urlpatterns = [
     path('posts/', views.post_view),
     path('hashtags/', views.hash_view),
